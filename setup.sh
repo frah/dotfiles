0c296434d1576c98c7d37369ddc3d9ed9691c82e
Atsushi OHNO <freiheit@tokcs.com>
2011-10-07 10:05:55 +0900

    セットアップをほぼ自動化

---
+++ setup.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+cd $(dirname $0)
+
+# Make dotfile links
+for dotfile in .?*
+do
+    if [ $dotfile != '..' ] && [[ $dotfile != '.git'* ]]
+    then
+        ln -Fis "$PWD/$dotfile" $HOME
+    fi
+done
+
+# Setup vim
+git submodule init
+git submodule update
+vim +BundleInstall
+
+HOOKSCRIPT_PATH='.vim/bundle/vimproc/.git/hooks/post-merge'
+cp .vim/vimproc_post-merge.sh $HOOKSCRIPT_PATH
+chmod +x $HOOKSCRIPT_PATH
+cd .vim/bundle/vimproc
+git pull
+
