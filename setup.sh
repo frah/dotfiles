d3c1b9f3422c2bd8246da7c56d9514468a80d4c1
frah <freiheit.tokkyu@gmail.com>
2017-03-24 15:21:13 +0900

    Update setup script

--- setup.sh
+++ setup.sh
@@ -11,21 +11,3 @@ do
     fi
 done
 
-# Setup vim
-git submodule init
-git submodule update
-echo -e "Installing bundles...\nAfter BundleInstall is complete, exit vim. (:qa!)" | vim +BundleInstall -
-
-cd ~/.vim/bundle/vimproc
-case "$(uname -s)" in
-Darwin)
-    make -f make_mac.mak
-    ;;
-FreeBSD)
-    make -f make_unix.mak
-    ;;
-esac
-
-cd ~/.vim/bundle/jcommenter.vim
-nkf -w -Lu --overwrite plugin/jcommenter.vim
-
