8f072781c1654f97f05fa0672a08b3fd59e332b6
frah <frah@tokcs.com>
2017-04-12 11:54:20 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,8 +5,14 @@ cd $(dirname $0)
 # Make dotfile links
 for dotfile in .?*
 do
-    if [ $dotfile != '..' ] && [[ $dotfile != '.git'* ]]
-    then
+    if [ $dotfile != '..' ] && [[ $dotfile != '.git'* ]]; then
+        if [ -e "$HOME/$dotfile" ]; then
+            if [ -L "$HOME/$dotfile" ]; then
+                rm -f $HOME/$dotfile
+            else
+                mv $HOME/$dotfile $HOME/${dotfile}_bk`date +%Y%m%d`
+            fi
+        fi
         ln -Fis "$PWD/$dotfile" $HOME
     fi
 done
